e0b03cfb72bbe6530ace3341e58550f4332d0cf1
kwasniew <kwasniewski.mateusz@gmail.com>
2019-12-02 10:06:54 +0100

    Convenient HTML building

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { h, app } from "https://unpkg.com/hyperapp@2.0.3";
+import htm from "https://unpkg.com/htm@2.2.1?module"
+
+const html = htm.bind(h);
 
 app({
-    view: state => h("div", {}, "text"),
+    view: state => html`<div>text</div>`,
     node: document.body
 });
