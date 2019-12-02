955440d5323f54f666f88411bea4d01a9b63e46f
kwasniew <kwasniewski.mateusz@gmail.com>
2019-12-02 10:59:18 +0100

    More advanced view

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ const Up = state => state + 1;
 app({
     init: state,
     view: state => html`
-          <div>
-            <h1>${state}</h1>
-            <button onclick=${Down}>-</button>
-            <button onclick=${Up}>+</button>
-          </div>
+          <section class="post">
+                <h1 class="post__title">A post about priorities and scheduling</h1>
+                <a href="#" class="post__link">http://example.com/post</a>
+                <div class="post__description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci mauris, euismod quis imperdiet eu, lacinia in leo. Mauris in orci sit amet eros finibus molestie. Vivamus laoreet, nibh eget ultrices consequat, leo mauris suscipit libero, eget feugiat enim turpis ac neque. Nam venenatis dolor ac posuere rhoncus.</p>
+                    <p>In eget finibus sapien, sit amet tempus augue. Pellentesque eu interdum nulla, sit amet dictum turpis. Sed ut lorem quis purus aliquet egestas. Nulla id risus ex. Morbi et lacinia risus. Integer eget ornare arcu. Donec purus est, dignissim a ante a, lacinia placerat massa. Donec quis lacinia lorem.</p>
+                </div>
+          </section>
     `,
     node: document.body
 });
