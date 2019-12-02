e25d274681ecdee16252757e3738d520663bd46f
kwasniew <kwasniewski.mateusz@gmail.com>
2019-12-02 10:17:58 +0100

    Triggering actions from view

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const html = htm.bind(h);
 
 const state = 0;
 
+const Down = state => state - 1;
+const Up = state => state + 1;
+
 app({
     init: state,
-    view: state => html`<div>${state}</div>`,
+    view: state => html`
+          <div>
+            <h1>${state}</h1>
+            <button onclick=${Down}>-</button>
+            <button onclick=${Up}>+</button>
+          </div>
+    `,
     node: document.body
 });
