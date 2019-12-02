75f86a22178df1a716aaeb815bace9ce2814a0d4
kwasniew <kwasniewski.mateusz@gmail.com>
2019-12-02 12:26:30 +0100

    ssr

---
+++ page.js
@@ -0,0 +1,45 @@
+import html from "./html.js";
+import { lazy } from "./lazy.js";
+import data from "./data.js";
+// import Comments from "./comments.js";
+
+const Comments = lazy(() => import("./comments.js"));
+
+export const state = {
+  showComments: false,
+  comments: data
+};
+
+const ToggleComments = state => ({
+  ...state,
+  showComments: !state.showComments
+});
+
+export const view = state => html`
+    <section class="post">
+      <h1 class="post__title">A post about priorities and scheduling</h1>
+      <a href="#" class="post__link">http://example.com/post</a>
+      <div class="post__description">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci
+          mauris, euismod quis imperdiet eu, lacinia in leo. Mauris in orci sit
+          amet eros finibus molestie. Vivamus laoreet, nibh eget ultrices
+          consequat, leo mauris suscipit libero, eget feugiat enim turpis ac
+          neque. Nam venenatis dolor ac posuere rhoncus.
+        </p>
+        <p>
+          In eget finibus sapien, sit amet tempus augue. Pellentesque eu
+          interdum nulla, sit amet dictum turpis. Sed ut lorem quis purus
+          aliquet egestas. Nulla id risus ex. Morbi et lacinia risus. Integer
+          eget ornare arcu. Donec purus est, dignissim a ante a, lacinia
+          placerat massa. Donec quis lacinia lorem.
+        </p>
+      </div>
+      <button onclick=${ToggleComments}>Toggle comments</button>
+      ${state.showComments
+  ? html`
+            <${Comments} comments=${state.comments} />
+          `
+  : ""}
+    </section>
+  `;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-import express from 'express';
-import compression from 'compression';
+import express from "express";
+import compression from "compression";
+import render from "hyperapp-render";
+import fs from "fs";
+import { state, view } from "./page.js";
 
 const server = express();
 const assetsDir = process.env.NODE_ENV === "production" ? "./dist" : ".";
-
+const htmlTemplate = fs.readFileSync(`${assetsDir}/index.html`, "UTF-8");
 server.use(compression());
+server.get("/ssr", function(req, res) {
+  const content = render.renderToString(view(state));
+  res.send(htmlTemplate.replace("<!-- PLACEHOLDER -->", content));
+});
 server.use(express.static(assetsDir));
 
-server.listen(3000, () => console.log('Example app listening on port 3000!'));
+server.listen(3000, () => console.log("Example app listening on port 3000!"));
