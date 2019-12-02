7b0a6db1d1b9243889a98492d5a6a05980dded0d
kwasniew <kwasniewski.mateusz@gmail.com>
2019-12-02 11:18:22 +0100

    vote down

--- vote-buttons.js
+++ vote-buttons.js
@@ -6,9 +6,15 @@ const VoteUp = index => state => {
   return newState;
 };
 
+const VoteDown = index => state => {
+  const newState = {...state};
+  newState.comments[index].score = newState.comments[index].score - 1;
+  return newState;
+};
+
 export const VoteButtons = ({ score, index }) => html`
   <div class="post__vote-buttons">
-    <button class="post__vote-down">-</button>
+    <button class="post__vote-down" onclick=${VoteDown(index)}>-</button>
     <span class="post__vote-score"
       >${score === 0 ? "0" : score > 0 ? "+" + score : score}</span
     >
