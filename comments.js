2fb92fbbb1cefc82c6d1c4ebf5df054ef24cfc40
kwasniew <kwasniewski.mateusz@gmail.com>
2019-12-02 13:27:10 +0100

    Memoization in practice

--- comments.js
+++ comments.js
@@ -1,11 +1,8 @@
 import html from "./html.js";
 import { VoteButtons } from "./vote-buttons.js";
+import {memoize} from "./memoize.js";
 
-const Comments = ({ comments, index }) => html`
-  <section class="post__comments">
-    <h1>Comments</h1>
-    ${comments.map((c, index) => {
-      return html`
+const Comment = (c, index) => html`
         <div class="post__comment">
           <h2 class="post__comment-author">${c.username} wrote</h2>
           <p class="post__comment-text">
@@ -14,7 +11,13 @@ const Comments = ({ comments, index }) => html`
           <${VoteButtons} index=${index} score=${c.score} />
         </div>
       `;
-    })}
+
+const memoizedComment = memoize(Comment);
+
+const Comments = ({ comments }) => html`
+  <section class="post__comments">
+    <h1>Comments</h1>
+    ${comments.map(memoizedComment)}
   </section>
 `;
 
--- memoize.js
+++ memoize.js
@@ -1,29 +1,11 @@
-var assert = require('assert');
-
-const memoize = fn => {
+export const memoize = (fn, keymaker = JSON.stringify) => {
   const lookupTable = {};
 
   return function (...args) {
-    const key = JSON.stringify(args);
+    const key = keymaker.apply(this, args);
 
-    return lookupTable[key] || (lookupTable[key] = fn(...args));
+    // console.log(lookupTable[key] ? "cache" : "miss");
+
+    return lookupTable[key] || (lookupTable[key] = fn.apply(this, args));
   };
 };
-
-var slowFibonacci = (n) =>
-  n < 2
-    ? n
-    : slowFibonacci(n - 2) + slowFibonacci(n - 1);
-
-var fastFibonacci = memoize(function (n) {
-  if (n < 2) {
-    return n;
-  }
-  else {
-    return fastFibonacci(n - 2) + fastFibonacci(n - 1);
-  }
-});
-
-
-// assert.equal(slowFibonacci(40), 102334155);
-assert.equal(fastFibonacci(40), 102334155);
